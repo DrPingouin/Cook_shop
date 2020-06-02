9efa8ab5687d625f3da5d01132caa6d715503b26
Paul-Edouard <paul.edouard.boudier@gmail.com>
2020-06-02 16:54:07 +0200

    changed field cannedfood

--- website/models.py
+++ website/models.py
@@ -6,14 +6,6 @@ class Type(models.Model):
     def __str__(self):
         return self.name
 
-class CannedFood(models.Model):
-    name = models.CharField(max_length=20)
-    price = models.IntegerField(default=1)
-    note = models.TextField(max_length=1000)
-
-    def __str__(self):
-        return self.name
-
 class Ingredient(models.Model):
     """
     We do not want to delete the Type object when an ingredient disappears
@@ -21,7 +13,15 @@ class Ingredient(models.Model):
     """
     name = models.CharField(max_length=20)
     type = models.ForeignKey(Type, blank=True, null=True, on_delete=models.PROTECT)
-    canned_food = models.ForeignKey(CannedFood, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class CannedFood(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField(default=1)
+    note = models.TextField(max_length=1000)
+    ingredients = models.ManyToManyField(Ingredient)
 
     def __str__(self):
         return self.name
