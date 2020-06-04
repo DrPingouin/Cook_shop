975b1429a6f17eb720d3b3b7a13187a6ec585827
DrPingouin <dylanblanchard178@gmail.com>
2020-06-04 15:52:05 +0200

    add search bar

--- website/views.py
+++ website/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
 
 from website.models import Type, CannedFood, Ingredient
 
 def welcome(request):
-    data = {}
+    if request.method == 'POST':
+        x = request.POST['canned_search']
+        data = {'canned_food': CannedFood.objects.filter(Q(ingredients__name__contains=x) | Q(name__contains=x)).distinct()}
+    else:
+        data = {'canned_food': CannedFood.objects.all()}
     return render(request, 'website/welcome.html', data)
-
 
 def types(request):
     data = {'types': Type.objects.all()}
     return render(request, 'website/types.html', data)
 
 def ingredients(request):
-    data = {'ingredients': Ingredient.objects.all()}
+    if request.method == 'POST':
+        x = request.POST['search']
+        data = {'ingredients': Ingredient.objects.filter(Q(type__name=x) | Q(name=x))}
+    else:
+        data = {'ingredients': Ingredient.objects.all()}
     return render(request, 'website/ingredients.html', data)
+
